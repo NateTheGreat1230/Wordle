5c5ab464042e80c32b02371e3334f82911944b4a
NateTheGreat <nathanprattwashburn@gmail.com>
2023-11-27 10:28:32 -0700

    add second word list, update logic to support that and add six fields to the wordle

--- src/main/java/com/wordle/wordle/GameLogic.java
+++ src/main/java/com/wordle/wordle/GameLogic.java
@@ -1,19 +1,23 @@
 package com.wordle.wordle;
 
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Scanner;
 
 public class GameLogic {
     String word;
+    boolean[] isTrue = new boolean[5];
     public String getWord() {
         if (word == null) {
             System.out.println("TESTING");
             setWord();
-            return word;
-        } else {
-            return word;
+            for (int i = 0; i < 5; i++) {
+                isTrue[i] = true;
+            }
         }
+        return word;
     }
     public String getGuess() {
         int lineNumber = (int) (Math.random() * 14855);
@@ -26,9 +30,9 @@ public class GameLogic {
         return line;
     }
     public void setWord() {
-        int lineNumber = (int) (Math.random()*14855);
+        int lineNumber = (int) (Math.random()*2315);
         try{
-            String line = Files.readAllLines(Paths.get("src/main/resources/com/wordle/wordle/Words.txt")).get(lineNumber);
+            String line = Files.readAllLines(Paths.get("src/main/resources/com/wordle/wordle/answers.txt")).get(lineNumber);
             word = line;
             System.out.println(line);
         }
@@ -38,12 +42,22 @@ public class GameLogic {
     }
     public boolean[] checkWord(String guess) {
         boolean[] correct = new boolean[5];
-        for (int i = 0; i < 5; i++) {
-            if (guess.charAt(i) == word.charAt(i)) {
-                correct[i] = true;
-            } else {
-                correct[i] = false;
+        File file = new File("src/main/resources/com/wordle/wordle/Words.txt");
+
+        try (Scanner scanner = new Scanner(file)) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (guess.equals(line)) {
+                    for (int j = 0; j < 5; j++) {
+                        correct[j] = guess.charAt(j) == word.charAt(j);
+                    }
+                    break;
+                } else {
+                    correct = new boolean[]{true, false, true, false, true};
+                }
             }
+        } catch (IOException e) {
+            e.printStackTrace();
         }
         return correct;
     }
--- src/main/java/com/wordle/wordle/WordleController.java
+++ src/main/java/com/wordle/wordle/WordleController.java
@@ -11,13 +11,37 @@ public class WordleController {
     GameLogic logic = new GameLogic();
     //null = logic.setWord();
     String word = logic.getWord();
+    //int scoreNum;
 
     @FXML
     private Label answer;
     @FXML
-    private Label right;
+    private Label right1;
+    @FXML
+    private Label right2;
+    @FXML
+    private Label right3;
+    @FXML
+    private Label right4;
+    @FXML
+    private Label right5;
+    @FXML
+    private Label right6;
+    @FXML
+    private Label score;
     @FXML
     private TextField guess1;
+    @FXML
+    private TextField guess2;
+    @FXML
+    private TextField guess3;
+    @FXML
+    private TextField guess4;
+    @FXML
+    private TextField guess5;
+    @FXML
+    private TextField guess6;
+
 
     @FXML
     protected void onHelloButtonClick() {
@@ -25,15 +49,32 @@ public class WordleController {
     }
     @FXML
     protected void onSubmit() {
-        boolean[] check = logic.checkWord(guess1.getText());
-        boolean[] isTrue = new boolean[5];
-        for (int i = 0; i < 5; i++) {
-            isTrue[i] = true;
-        }
-        if (Arrays.equals(check, isTrue)) {
-            right.setText("Correct!");
+        boolean[] check1 = logic.checkWord(guess1.getText());
+        boolean[] check2 = logic.checkWord(guess2.getText());
+        boolean[] check3 = logic.checkWord(guess3.getText());
+        boolean[] check4 = logic.checkWord(guess4.getText());
+        boolean[] check5 = logic.checkWord(guess5.getText());
+        boolean[] check6 = logic.checkWord(guess6.getText());
+        if (Arrays.equals(check1, logic.isTrue)) {
+            score.setText("You got it in " + 1 + " guess.");
+        } else if (Arrays.equals(check2, logic.isTrue)) {
+            score.setText("You got it in " + 2 + " guesses.");
+        } else if (Arrays.equals(check3, logic.isTrue)) {
+            score.setText("You got it in " + 3 + " guesses.");
+        } else if (Arrays.equals(check4, logic.isTrue)) {
+            score.setText("You got it in " + 4 + " guesses.");
+        } else if (Arrays.equals(check5, logic.isTrue)) {
+            score.setText("You got it in " + 5 + " guesses.");
+        } else if (Arrays.equals(check6, logic.isTrue)) {
+            score.setText("You got it in " + 6 + " guesses.");
         } else {
-            right.setText("Incorrect.");
+            score.setText("Not the correct word.");
+            right1.setText(Arrays.toString(check1));
+            right2.setText(Arrays.toString(check2));
+            right3.setText(Arrays.toString(check3));
+            right4.setText(Arrays.toString(check4));
+            right5.setText(Arrays.toString(check5));
+            right6.setText(Arrays.toString(check6));
         }
     }
 }
